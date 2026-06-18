e32eda121532b036c7aeaaa6a6d869bcc261502a
mufy <mufajjul.ali@gmail.com>
2026-06-18 11:12:32 +0100

    Updated

--- infra/Databricks/ARM/cli.sh
+++ infra/Databricks/ARM/cli.sh
@@ -1,10 +1,11 @@
 
-az login --tenant 16b3c013-d300-468d-ac64-7eda0820b6d3 
+az login 
+#--tenant 16b3c013-d300-468d-ac64-7eda0820b6d3 
 
 # Create a resource group
-az group create \
-  --name pep-azr-aisp-msft-training-lab-sbx-eus-01-rg \
-  --location swedencentral
+#az group create \
+#  --name pep-azr-aisp-msft-training-lab-sbx-eus-01-rg \
+#  --location swedencentral
 
 az deployment group create \
   --resource-group pep-azr-aisp-msft-training-lab-sbx-eus-01-rg \
