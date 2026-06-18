32125beca34b50d8a183ad2235fa605baaddf739
mufy <mufajjul.ali@gmail.com>
2026-06-18 11:09:20 +0100

    updated script

--- infra/Databricks/ARM/cli.sh
+++ infra/Databricks/ARM/cli.sh
@@ -1,13 +1,13 @@
 
-#az login --tenant 16b3c013-d300-468d-ac64-7eda0820b6d3 
+az login --tenant 16b3c013-d300-468d-ac64-7eda0820b6d3 
 
 # Create a resource group
 az group create \
-  --name rg-dbx-hybrid-rs \
-  --location eastus2
+  --name pep-azr-aisp-msft-training-lab-sbx-eus-01-rg \
+  --location swedencentral
 
 az deployment group create \
-  --resource-group rg-dbx-hybrid-rs \
+  --resource-group pep-azr-aisp-msft-training-lab-sbx-eus-01-rg \
   --template-file databricks-hybrid.json \
   --parameters @databricks-hybrid.parameters.json
 
